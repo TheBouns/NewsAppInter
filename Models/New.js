8a8bccdbe71f7be5421fdab424a80c2a92f61534
TheBouns <ilbonsi3@hotmail.com>
2022-05-09 02:51:21 +0200

    deleted all about users: model,routes,controller

--- Models/New.js
+++ Models/New.js
@@ -3,15 +3,18 @@ const mongoose = require("mongoose");
 const NewsSchema = new mongoose.Schema({
   title: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   date: {
     type: Date,
   },
   content: {
     type: String,
+    required: true,
   },
   author: {
     type: String,
--- Routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const UserController = require("../controllers/UserController");
-
-router.post("/register", UserController.createUser);
-router.get("/login", UserController.login);
-router.get("/find", UserController.getAllUsers);
-
-module.exports = router;
